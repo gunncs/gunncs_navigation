43f1d0636cce95f2cc9e1be97c23ee6c1fb654b5
Andrew Chen <andrewtheannihilator@gmail.com>
2012-04-24 12:44:42 -0700

    graphing delta R

--- nodes/laserAngle.py
+++ nodes/laserAngle.py
@@ -17,23 +17,35 @@ import numpy as np
 import pyqtgraph as pg
 
 
+
+
 def main():
     rospy.loginfo("starting laserAngle")
-    global pub, window, win, plot, curve
+    #global window, win, plot, rangeCurve
     #open plotter
     rospy.loginfo("starting plotter...")
+
+    global rangeCurve, errorCurve
     app = QtGui.QApplication([])
     
     win = pg.GraphicsWindow(title="Basic plotting examples")
     win.resize(800,600)
 
-    plot = win.addPlot(title="Ranges")
-    plot.enableAutoRange('xy', True)
-    curve = plot.plot(pen='y')
+    rangePlot = win.addPlot(title="Ranges")
+    rangePlot.enableAutoRange('xy', True)
+    rangeCurve = rangePlot.plot(pen='y')
+
+    errorPlot = win.addPlot(title="delta R")
+    #errorPlot.enableAutoRange('xy', True)
+    #errorPlot.setYRange(0, .05)
+    errorPlot.setYRange(0, 1)
+    errorPlot.setXRange(120, 230)
+    errorCurve = errorPlot.plot(pen='y')
 
 
     #open vis
     rospy.loginfo("starting visualization...")
+    global window
     pygame.init()
     window = pygame.display.set_mode((640,480))
 
@@ -44,27 +56,78 @@ def main():
     app.exec_()
     rospy.spin()
 
-def scanned(data):
-    global pub, window, win, plot, curve
+def scanned(laserscan):
+    global  window, win, rangeCurve
 
     #update plot
-    curve.setData(np.array(data.ranges))
+    rangeCurve.setData(np.array(laserscan.ranges))
 
     #draw vis
+    drawVis(laserscan)
+    calculateError(laserscan)
+
+def getNeighborDistance(laserscan, id, neighborRange):
+    mydistance = laserscan.ranges[id]
+    error = 0
+    lowerbound = (id - neighborRange) % len(laserscan.ranges)
+    upperbound = (id + neighborRange) % len(laserscan.ranges)
+    for i in range(lowerbound, upperbound):
+        error += mydistance - laserscan.ranges[i]
+
+    return math.fabs(error / ((2 * neighborRange) + 1))
+        
+
+def filter(laserscan):
+    last_range = 0
+    for i in range(0, len(laserscan.ranges)-1):
+        error = getNeighborDistance(laserscan, i, 5)
+
+        
+def calculateError(laserscan):
+    #print laserscan
+    global errorCurve
+    #print(calculateSlope(laserscan, 200, 210))
+
+    
+    errors= []
+
+    for i in range(0, len(laserscan.ranges)):
+        errors.append(getNeighborDistance(laserscan, i, 2))
+
+    errorCurve.setData(np.array(errors))
+    
+
+
+        #print(str(p1) + "\t" + str(p1[0]))
+def calculateSlope(laserscan, n1, n2):
+    p1 = getPoint(laserscan, n1)
+    p2 = getPoint(laserscan, n2)
+    m = (p2[1] - p1[1]) / (p2[0] - p1[0])
+    return m
+
+    
+
+def getPoint(laserscan, n):
+    theta = laserscan.angle_min + laserscan.angle_increment * n
+    x = laserscan.ranges[n] * math.cos(theta)
+    y = laserscan.ranges[n] * math.sin(theta)
+    return (x, y)
+
+def drawVis(laserscan):
+    global window
     pygame.draw.rect(window, (0,0,0), (0, 0, 640, 480))
     pygame.draw.circle(window, (255,0,0), (320, 240), 20, 0)
 
-    for i in range(0, len(data.ranges)):
-        theta = data.angle_min + data.angle_increment * i
-        x = 320 + 100 *data.ranges[i] * math.cos(theta)
+    for i in range(0, len(laserscan.ranges)):
+        #dont know why we need the negative sign...
+        theta = -(laserscan.angle_min + laserscan.angle_increment * i)
+        x = 320 + 100 *laserscan.ranges[i] * math.cos(theta)
 
-        y = 240 + 100 *data.ranges[i] * math.sin(theta)
+        y = 240 + 100 *laserscan.ranges[i] * math.sin(theta)
         
         pygame.draw.circle(window, (0,255,0), (int(x), int(y)), 1, 0)
 
     pygame.display.flip()
-
-
 
 
 if __name__ == "__main__":
