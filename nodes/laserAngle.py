1e9c59a7bf151d99eb8d06d7a79fef8b84faa3ae
Andrew Chen <andrewtheannihilator@gmail.com>
2012-04-24 23:05:10 -0700

    linear clustering colorized

--- nodes/laserAngle.py
+++ nodes/laserAngle.py
@@ -6,6 +6,7 @@ import serial
 import sys
 import pygame
 import math
+import random
 
 from std_msgs.msg import *
 from geometry_msgs.msg import *
@@ -17,6 +18,7 @@ import pyqtgraph as pg
 
 
 NEIGHBOR_RANGE= 2
+COLORS = {}
 
 def main():
     rospy.loginfo("starting laserAngle")
@@ -38,7 +40,7 @@ def main():
     #errorPlot.enableAutoRange('xy', True)
     #errorPlot.setYRange(0, .05)
     errorPlot.setYRange(0, .2)
-    errorPlot.setXRange(120, 230)
+    errorPlot.setXRange(120, 250)
     errorCurve = errorPlot.plot(pen=(255, 0,0))
     centersCurve = errorPlot.plot(pen=(0,255,0))
 
@@ -47,7 +49,7 @@ def main():
     rospy.loginfo("starting visualization...")
     global window
     pygame.init()
-    window = pygame.display.set_mode((640,480))
+    window = pygame.display.set_mode((640,640))
 
     rospy.loginfo("subscribing to laser...")
     rospy.init_node('laserAngle')
@@ -67,12 +69,43 @@ def scanned(laserscan):
     rangeCurve.setData(np.array(laserscan.ranges))
 
     #draw vis
-    drawVis(laserscan)
     errors = calculateError(laserscan)
-    filterError(errors)
+    intersects = getIntersects(errors)
+    clusters = getClusters(intersects)
+    drawVis(laserscan, clusters)
+    #print(intersects)
 
-def filterError(errors):
+def getClusters(positions):
+    '''
+    given a list of points, points are clustered.
+    we return the positions of the centers of these clusters.
+    '''
 
+    clusters= [] #to return
+    currentClusterStart = -1 #we start without a cluster
+    previousPos = positions[0] -1 # we force the first item to be a cluster
+
+
+    for position in positions:
+        #if we see that we are continuous,
+        if previousPos + 1 is position:
+            #and this is the first time, we record this position.
+            if currentClusterStart is -1:
+                currentClusterStart= previousPos
+        #if we lost continuity,
+        else:
+            #we end the cluster
+            clusters.append([currentClusterStart , previousPos])
+            # and reset
+            currentClusterStart = -1
+                
+        previousPos = position
+    #add the last cluster set, that isn't iterated
+    clusters.append([currentClusterStart , previousPos])
+
+    return clusters
+
+def getIntersects(errors):
     global centersCurve
 
     positions = []
@@ -82,12 +115,13 @@ def filterError(errors):
             positions.append(i)
             values.append(.03)
         else:
-            positions.append(i)
             values.append(0)
 
     #print(positions)
 
-    centersCurve.setData(np.array(positions), np.array(values))
+    centersCurve.setData(np.array(values))
+
+    return positions
 
 
 def getNeighborDistance(laserscan, id, neighborRange):
@@ -129,10 +163,7 @@ def calculateError(laserscan):
     #print laserscan
     global errorCurve, NEIGHBOR_RANGE
     #print(calculateSlope(laserscan, 200, 210))
-
-    
     errors= []
-
     for i in range(0, len(laserscan.ranges)):
         errors.append(getNeighborDistance(laserscan, i, NEIGHBOR_RANGE))
 
@@ -156,21 +187,41 @@ def getPoint(laserscan, n):
     y = laserscan.ranges[n] * math.sin(theta)
     return (x, y)
 
-def drawVis(laserscan):
+def drawVis(laserscan, bounds):
     global window
-    pygame.draw.rect(window, (0,0,0), (0, 0, 640, 480))
-    pygame.draw.circle(window, (255,0,0), (320, 240), 20, 0)
+    pygame.draw.rect(window, (0,0,0), (0, 0, 640, 640))
+    pygame.draw.circle(window, (255,0,0), (0, 320), 20, 0)
 
+
+    
     for i in range(0, len(laserscan.ranges)):
+
+        if i is (bounds[bound % len(bounds)])[1]:
+            color = nonRandomColor(bound)
+            bound = bound + 1
+
         #dont know why we need the negative sign...
         theta = -(laserscan.angle_min + laserscan.angle_increment * i)
-        x = 320 + 100 *laserscan.ranges[i] * math.cos(theta)
+        x = 0 + 300 *laserscan.ranges[i] * math.cos(theta)
 
-        y = 240 + 100 *laserscan.ranges[i] * math.sin(theta)
+        y = 320 + 300 *laserscan.ranges[i] * math.sin(theta)
         
-        pygame.draw.circle(window, (0,255,0), (int(x), int(y)), 1, 0)
+        pygame.draw.circle(window, color, (int(x), int(y)), 1, 0)
 
     pygame.display.flip()
+
+
+
+def nonRandomColor(i):
+    global COLORS
+    if i not in COLORS:
+        COLORS[i] = (
+                random.randint(0,255),
+                random.randint(0,255),
+                random.randint(0,255))
+    return COLORS[i]
+
+
 
 
 if __name__ == "__main__":
