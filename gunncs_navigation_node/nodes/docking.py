7d02ce92e8befde3eea3767d1656b6869480abb4
Dan Frei <frei.daniel3@gmail.com>
2012-06-22 23:24:21 -0700

    add a docking pub

--- gunncs_navigation_node/nodes/docking.py
+++ gunncs_navigation_node/nodes/docking.py
@@ -6,34 +6,36 @@ import rospy
 
 from geometry_msgs.msg import *
 from turtlebot_node.msg import *
+from std_msgs.msg import *
 
 def main():
-    global pub
+    global pub, pubDocking
     rospy.loginfo("waiting for dock IR")
     rospy.init_node('docking')
     rospy.Subscriber("/turtlebot_node/sensor_state", TurtlebotSensorState, sensStateChange)
     pub = rospy.Publisher("/cmd_vel", Twist)
+    pubDocking = rospy.Publisher("/Docking", Bool)
     rospy.spin()
 
 def sensStateChange(data):
     ir = data.remote_opcode
     if (ir == data.REMOTE_NONE):
-	    return
+        return
     else: 
-	    rospy.loginfo("detected IR forcefield!")
-	    msg = Twist()
-    	    if (ir >= data.REMOTE_RED_BUOY_AND_GREEN_BUOY):
-		msg.linear.x = 0.05
-		msg.angular.z = 0.0
-		pub.publish(msg)
-	    elif (ir >= data.REMOTE_RED_BUOY):
-	    	msg.linear.x = 0.05
-		msg.angular.z = 0.4
-		pub.publish(msg)
-	    elif (ir >= data.REMOTE_GREEN_BUOY):
-	    	msg.linear.x = 0.05
-		msg.angular.z = -0.4
-		pub.publish(msg)
+        rospy.loginfo("detected IR forcefield!")
+        msg = Twist()
+        if (ir >= data.REMOTE_GREEN_BUOY):
+               if (ir >= data.REMOTE_RED_BUOY_AND_GREEN_BUOY):
+                msg.linear.x = 0.05
+                msg.angular.z = 0.0
+            elif (ir >= data.REMOTE_RED_BUOY):
+                msg.linear.x = 0.05
+                msg.angular.z = 0.4
+            else:
+                msg.linear.x = 0.05
+                msg.angular.z = -0.4
+            pub.publish(msg)
+            pubDocking.publish(True)
 
 
 
